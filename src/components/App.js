5d6f69c8a1e5919e6805547ae93ebd1a67c8a69b
Daryl Chan <dckj95@gmail.com>
2017-11-04 13:38:05 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// External imports
+import React from 'react';
+
+// Component
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Discovery</h1>
+    );
+  }
+}
+
+// Export
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+// External imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Internal imports
+import App from './components/App'
+
+// Styles
+import './styles/index.css'
+
+// Render
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
