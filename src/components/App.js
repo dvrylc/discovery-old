c34a9642826186922d61c5effdc2643e2f93e0da
Daryl Chan <dckj95@gmail.com>
2017-11-05 08:41:58 +0800

    Updated links for item view

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 // Internal imports
 import Header from './Header';
 import ArticleList from './ArticleList';
+import Item from './Item';
 
 // Component
 class App extends React.Component {
@@ -13,7 +14,8 @@ class App extends React.Component {
       <div className="app">
         <Header />
 
-        <Route path="/" component={ ArticleList } />
+        <Route exact path="/" component={ ArticleList } />
+        <Route path="/item/:id" component={ Item } />
       </div>
     );
   }
--- src/components/ArticleLink.js
+++ src/components/ArticleLink.js
@@ -1,8 +1,11 @@
 // External imports
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ArticleLink extends React.Component {
   render() {
+    const url = '/item/' + this.props.article.id;
+
     return (
       <article>
         <span>{this.props.article.user} | {this.props.article.time_ago}</span>
@@ -12,12 +15,16 @@ class ArticleLink extends React.Component {
         </h1>
 
         <span>
-          <i className="fa fa-caret-up" aria-hidden="true"></i>  {this.props.article.points} <i className="fa fa-commenting-o" aria-hidden="true"></i> {this.props.article.comments_count}
+          <Link to={{ pathname: url, state: { article: this.props.article }}}>
+            <i className="fa fa-caret-up" aria-hidden="true"></i>  {this.props.article.points}
+
+            <i className="fa fa-commenting-o" aria-hidden="true"></i> {this.props.article.comments_count}
+          </Link>
         </span>
       </article>
     );
   }
 }
 
-// Export 
+// Export
 export default ArticleLink;
